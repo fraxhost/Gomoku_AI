c95977f82dcd353f9d02802cd01b247032ea9329
Hasnatul Jelan <bsse1026@iit.du.ac.bd>
2021-02-01 20:38:59 +0600

    closing to an end

--- lib/gameCore/goLogic.dart
+++ lib/gameCore/goLogic.dart
@@ -3,210 +3,223 @@ import 'package:go_game/boardstate.dart';
 class GoLogic{
 
   int rows = 10, coloumns = 10;
+  List<List<int>> board;
 
-  bool fiveInA_row(int player){
+  int fiveInA_row(int player){
 
-    BoardState board = new BoardState();
-    List<List<int>> currentBoard = board.getBoard();
     int stones=0;
 
     for(int i = 0; i < rows; i++)
       for(int j=0; j <= coloumns-5; j++){
-
-        if(currentBoard[i][j] == player){
+        stones = 0;
+        if(board[i][j] == player){
           stones+= 1;
           for(int k =1; k<5; k++){
-            if(currentBoard[i][++j] == player) {
+            if(board[i][j+k] == player) {
               stones += 1;
             }
             else
               break;
           }
-          if(stones == 5)
-            return true;
+          if(stones == 5){
+            print('row win');
+            if(BoardState.board[i][j] == null)
+              return i*10+j;
+            else
+              return i*10+j+5;
+          }
           else
             continue;
-          stones = 0;
+
         }
       }
-
-    return false;
+    return -1;
   }
 
-  bool fiveInA_column(int player){
+  int fiveInA_column(int player){
 
-    BoardState board = new BoardState();
-
-    List<List<int>> currentBoard = board.getBoard();
     int stones=0;
-
+    int k;
     for(int i = 0; i < coloumns; i++)
       for(int j=0; j <= rows-5; j++){
-
-        if(currentBoard[i][j] == player){
+        stones = 0;
+        if(board[j][i] == player){
           stones+= 1;
-          for(int k =1; k<5; k++){
-            if(currentBoard[i][++j] == player) {
+          for(k =1; k<5; k++){
+            if(board[j+k][i] == player) {
               stones += 1;
             }
             else
               break;
           }
-          if(stones == 5)
-            return true;
+          if(stones == 5){
+            print('col win');
+            if(BoardState.board[j][i] == null)
+            return j*10+i;
+            else
+              return j*10+i+40;
+          }
           else
             continue;
-          stones = 0;
         }
       }
-    return false;
+    return -1;
   }
 
-  bool fiveInA_leftDiagonal(int player){
+  int fiveInA_leftDiagonal(int player){
 
-    BoardState board = new BoardState();
+    int stones=0;
+    int critcalMove = -1;
 
-    List<List<int>> currentBoard = board.getBoard();
+    for(int i = 0; i <= rows-5; i++)
+      for(int j=4; j < coloumns; j++){
+        stones = 0;
+        if(board[i][j] == player){
+          stones+= 1;
+          for(int k =1; k<5; k++){
+            if(board[i+k][j-k] == player) {
+              stones += 1;
+            }
+            else
+              break;
+          }
+          if(stones == 5){
+            //print('leftwin');
+            return i*10+j;
+          }
+          else
+            continue;
+
+        }
+      }
+    return -1;
+  }
+  int fiveInA_RightDiagonal(int player){
+
     int stones=0;
 
     for(int i = 0; i <= rows-5; i++)
       for(int j=0; j <= coloumns-5; j++){
-
-        if(currentBoard[i][j+4] == player){
+        stones = 0;
+        if(board[i][j] == player){
           stones+= 1;
           for(int k =1; k<5; k++){
-            if(currentBoard[i+1][j-k+4] == player) {
-              stones += 1;
+            if(board[i+k][j+k] == player) {
+              //print('${j} ${i}');
+              stones+=1;
             }
             else
               break;
           }
-          if(stones == 5)
-            return true;
-          else
-            continue;
-          stones = 0;
-        }
-      }
-    return false;
-  }
-  bool fiveInA_RightDiagonal(int player){
-
-    BoardState board = new BoardState();
-
-    List<List<int>> currentBoard = board.getBoard();
-    int stones=0;
-
-    for(int i = 0; i <= rows-5; i++)
-      for(int j=0; j <= coloumns-5; j++){
-
-        if(currentBoard[i][j] == player){
-          stones+= 1;
-          for(int k =1; k<5; k++){
-            if(currentBoard[i+1][j+1] == player) {
-              stones += 1;
-            }
-            else
-              break;
+          if(stones == 5) {
+            //print('rightwin');
+            return i*10+j;
           }
-          if(stones == 5)
-            return true;
           else
             continue;
-          stones = 0;
         }
       }
-    return false;
+    return -1;
   }
 
-  bool findTwoOpenEndInRow(int stones, int playerStone){
+  int findTwoOpenEndInRow(int stones, int playerStone){
 
     for(int r=0;r<rows;r++){
       for(int col = 0; col < coloumns-stones-1; col++){
-
-        if(BoardState.board[r][col]==null && BoardState.board[r][col+stones+1]==null){
+        //print('TwoInRow ${r} ${col}');
+        if(board[r][col]==null && board[r][col+stones+1]==null){
           bool pattern = true;
           for(int k= col+1; k <= col+stones; k++){
-            if(BoardState.board[r][k] != playerStone){
+            if(board[r][k] != playerStone){
               pattern = false;
               break;
             }
           }
-          if(pattern)
-            return true;
+          if(pattern){
+            //print('two in row');
+            return r*10+col;
+          }
         }
       }
     }
-   return false;
+   return -1;
   }
 
-  bool findTwoOpenEndInColumn(int stones, int playerStone){
+  int findTwoOpenEndInColumn(int stones, int playerStone){
 
     for(int col=0;col<coloumns;col++){
       for(int r = 0; r < rows-stones-1; r++){
-        if(BoardState.board[col][r] == null && BoardState.board[col][r+stones+1] == null){
+      //  print('TwoInColumn ${r} ${col}');
+        if(board[r][col] == null && board[r+stones+1][col] == null){
           bool pattern = true;
           for(int k= r+1; k <= r+stones; k++ ){
-            if(BoardState.board[k][col] != playerStone){
+            if(board[k][col] != playerStone){
               pattern = false;
               break;
             }
           }
-          if(pattern)
-            return true;
+          if(pattern) {
+            //print('two in col');
+            return r*10+col;
+          }
         }
       }
     }
-    return false;
+    return -1;
   }
-  bool findTwoOpenEndInRightDiagonal(int stones, int playerStone){
+  int findTwoOpenEndInRightDiagonal(int stones, int playerStone){
 
     for(int r = 0; r < rows-stones-1; r++){
       for(int col = 0; col < coloumns-stones-1; col++){
-        if(BoardState.board[r][col] == null && BoardState.board[r][col]== BoardState.board[r+stones+1][col+stones+1]){
+       // print('TwoInRight ${r} ${col}');
+        if(board[r][col] == null && board[r+stones+1][col+stones+1] == null){
           bool pattern = true;
           for(int k = 1; k <= stones;k++) {
-            if(BoardState.board[r+k][col+k] != playerStone){
+            if(board[r+k][col+k] != playerStone){
               pattern = false;
               break;
             }
           }
-          if(pattern)
-            return true;
+          if(pattern) {
+            //print('two in right');
+            return r*10+col;
+          }
         }
       }
     }
-    return false;
+    return -1;
   }
 
-  bool findTwoOpenEndInlLeftDiagonal(int stones, int playerStone){
+  int findTwoOpenEndInLeftDiagonal(int stones, int playerStone){
 
     for(int r = 0; r < rows-stones-1; r++){
       for(int col = stones+1; col < coloumns; col++){
-        if(BoardState.board[r][col] == null && BoardState.board[r+stones+1][col-stones-1] == null){
+       // print('TwoInLeft ${r} ${col}');
+        if(board[r][col] == null && board[r+stones+1][col-stones-1] == null){
           bool pattern = true;
           for(int k = 1; k <= stones;k++) {
-            if(BoardState.board[r+k][col-k] != playerStone){
+            if(board[r+k][col-k] != playerStone){
               pattern = false;
               break;
             }
           }
-          if(pattern)
-            return true;
+          if(pattern){
+            return r*10+col;
+          }
         }
       }
     }
-    return false;
+    return -1;
   }
   bool findOneOpenEndInRow(int stones, int playerStone){
 
-    for(int r=0; r < rows; r++){
-      for(int col =0; col < coloumns-stones; col++){
-
-        if(BoardState.board[r][col] == null){
+    for(int r= 0; r < rows; r++){
+      for(int col = 0; col < coloumns-stones; col++){
+       // print('OneInRow ${r} ${col}');
+        if(board[r][col] == null){
           bool pattern = true;
-            for(int k= col + 1; k <= col + stones; col++){
-                if(BoardState.board[r][k] != playerStone){
+            for(int k= col + 1; k <= col + stones; k++){
+                if(board[r][k] != playerStone){
                   pattern = false;
                   break;
                 }
@@ -216,10 +229,11 @@ class GoLogic{
         }
       }
       for(int colRev = coloumns-1; colRev >= coloumns-stones; colRev--){
-        if(BoardState.board[r][colRev]==null){
+       // print('OneInRowRev ${r} ${colRev}');
+        if(board[r][colRev]==null){
           bool pattern = true;
           for(int k = colRev-stones; k < colRev; k++){
-            if(BoardState.board[r][k] != playerStone){
+            if(board[r][k] != playerStone){
               pattern = false;
               break;
             }
@@ -236,44 +250,50 @@ class GoLogic{
 
     for(int col=0; col < coloumns; col++){
       for(int r =0; r < rows-stones; r++){
-
-        if(BoardState.board[r][col] == null){
+        //print('OneInCol ${r} ${col}');
+        if(board[r][col] == null){
           bool pattern = true;
           for(int k= r + 1; k <= r + stones; k++){
-            if(BoardState.board[k][col] != playerStone){
+            if(board[k][col] != playerStone){
               pattern = false;
               break;
             }
           }
-          if(pattern)
+          if(pattern) {
+           // print('col 4 1 found');
             return true;
+          }
         }
       }
       for(int rowRev = rows-1; rowRev >= rows-stones; rowRev--){
-        if(BoardState.board[rowRev][col]==null){
+       // print('OneInColRev ${rowRev} ${col}');
+        if(board[rowRev][col]==null){
           bool pattern = true;
           for(int k = rowRev-stones; k < rowRev; k++){
-            if(BoardState.board[k][col] != playerStone){
+            if(board[k][col] != playerStone){
               pattern = false;
               break;
             }
           }
-          if(pattern)
+          if(pattern) {
+           // print('col 4 1 found');
             return true;
+          }
         }
       }
     }
     return false;
   }
 
-  bool findOneOpenEndInlLeftDiagonal(int stones, int playerStone){
+  bool findOneOpenEndInLeftDiagonal(int stones, int playerStone){
 
     for(int r = 0; r < rows-stones; r++){
       for(int col = stones; col < coloumns; col++){
-        if(BoardState.board[r][col] == null){
+       // print('OneInLeft ${r} ${col}');
+        if(board[r][col] == null){
           bool pattern = true;
           for(int k = 1; k <= stones;k++) {
-            if(BoardState.board[r+k][col-k] != playerStone){
+            if(board[r+k][col-k] != playerStone){
               pattern = false;
               break;
             }
@@ -281,10 +301,11 @@ class GoLogic{
           if(pattern)
             return true;
         }
-        else if(BoardState.board[r+stones][col-stones] == null){
+        else if(board[r+stones][col-stones] != playerStone){
+         // print('OneInLeft ${r} ${col}');
           bool pattern = true;
           for(int k= 0; k<stones; k++){
-            if(BoardState.board[r+k][col-k] != playerStone){
+            if(board[r+k][col-k] != playerStone){
               pattern =false;
               break;
             }
@@ -297,25 +318,29 @@ class GoLogic{
     return false;
   }
 
-  bool findOneOpenEndInlRightDiagonal(int stones, int playerStone){
+  bool findOneOpenEndInRightDiagonal(int stones, int playerStone){
 
     for(int r = 0; r < rows-stones; r++){
       for(int col = 0; col < coloumns-stones; col++){
-        if(BoardState.board[r][col] == null){
+        //print('OneInRight ${r} ${col}');
+        if(board[r][col] == null){
           bool pattern = true;
           for(int k = 1; k <= stones;k++) {
-            if(BoardState.board[r+k][col-k] != playerStone){
+            if(board[r+k][col+k] != playerStone){
               pattern = false;
               break;
             }
           }
-          if(pattern)
+          if(pattern){
+            //print('right dia found');
             return true;
+          }
         }
-        else if(BoardState.board[r+stones][col+stones] == null){
+        else if(board[r+stones][col+stones] == null){
+         // print('OneInRight ${r} ${col}');
           bool pattern = true;
           for(int k= 0; k<stones; k++){
-            if(BoardState.board[r+k][col+k] != playerStone){
+            if(board[r+k][col+k] != playerStone){
               pattern =false;
               break;
             }
@@ -327,4 +352,148 @@ class GoLogic{
     }
     return false;
   }
+
+  int searchForTwoOpenEnd(int stones, int playerStone, List<List<int>> cBoard){
+
+    board = cBoard;
+    //return (findTwoOpenEndInColumn(stones, playerStone) || findTwoOpenEndInRightDiagonal(stones, playerStone) ||
+      //  findTwoOpenEndInLeftDiagonal(stones, playerStone));
+    int critcalMove = findTwoOpenEndInRow(3,1);
+    if(critcalMove>-1)
+      return critcalMove;
+    critcalMove = findTwoOpenEndInColumn(3,1);
+    if(critcalMove>-1)
+      return critcalMove;
+    critcalMove = findTwoOpenEndInLeftDiagonal(3,1);
+    if(critcalMove>-1)
+      return critcalMove;
+    critcalMove = findTwoOpenEndInRightDiagonal(3,1);
+    if(critcalMove>-1)
+      return critcalMove;
+
+    return -1;
+  }
+
+  bool searchForOneOpenEnd(int stones, int playerStone, List<List<int>> cBoard){
+
+    board = cBoard;
+   // return (findOneOpenEndInColumn(stones, playerStone) || findOneOpenEndInRightDiagonal(stones, playerStone)||
+   // findOneOpenEndInLeftDiagonal(stones, playerStone));
+    return (findOneOpenEndInRow(stones, playerStone) || findOneOpenEndInColumn(stones, playerStone) ||
+        findOneOpenEndInLeftDiagonal(stones, playerStone) || findOneOpenEndInRightDiagonal(stones, playerStone));
+  }
+  int highFiveAfter(int player){
+
+    board = BoardState.board;
+
+    int critcalMove = fiveInA_row(player);
+    if(critcalMove>-1)
+      return critcalMove;
+    critcalMove = fiveInA_column(player);
+    if(critcalMove>-1)
+      return critcalMove;
+    critcalMove = fiveInA_leftDiagonal(player);
+    if(critcalMove>-1)
+      return critcalMove;
+    critcalMove = fiveInA_RightDiagonal(player);
+    if(critcalMove>-1)
+      return critcalMove;
+    return -1;
+  }
+
+  int highFive(List<List<int>> recievedBoard, int player){
+
+    board = recievedBoard;
+
+    int critcalMove = fiveInA_row(player);
+    if(critcalMove>-1) {
+      return critcalMove;
+    }
+    critcalMove = fiveInA_column(player);
+    if(critcalMove>-1)
+      return critcalMove;
+    critcalMove = fiveInA_leftDiagonal(player);
+    if(critcalMove>-1)
+      return critcalMove;
+    critcalMove = fiveInA_RightDiagonal(player);
+    if(critcalMove>-1)
+      return critcalMove;
+
+    return -1;
+  }
+
+
+  int evaluateGameTree(bool AI_turn, List<List<int>> cboard){
+
+    board = cboard;
+    int sign = (AI_turn)? 1 : -1;
+    int myStone = (AI_turn)? 0 : 1;
+    int opponentStone = (myStone == 0)? 1 : 0;
+
+    int value =0;
+
+    /*value += (searchForTwoOpenEnd(4, 0))?100000*1:1;
+    value += (searchForOneOpenEnd(4, 0))?550000*1:1;
+    value += (searchForTwoOpenEnd(3, 0))?540*1:1;
+    value += (searchForOneOpenEnd(3, 0))?50*1:1;
+    value += (searchForTwoOpenEnd(2, 0))?10*1:1;
+    value += (searchForOneOpenEnd(2, 0))?5*1:1;
+
+    value -= (searchForTwoOpenEnd(4, 1))?101000*1:1;
+    value -= (searchForOneOpenEnd(4, 1))?510*1:1;
+    value -= (searchForTwoOpenEnd(3, 1))?530*1:1;
+    value -= (searchForOneOpenEnd(3, 1))?60*1:1;
+    value -= (searchForTwoOpenEnd(2, 1))?15*1:1;
+    value -= (searchForOneOpenEnd(2, 1))?8*1:1;
+
+    return value;
+    if(highFive(0))
+      return 1000000000;
+    if(highFive(1))
+      return -1000000000;*/
+
+    if(searchForTwoOpenEnd(4, myStone,board)>-1)
+      return sign*100000000;
+    if(searchForTwoOpenEnd(4, opponentStone,board)>-1)
+     return -sign*500000;
+
+    if(searchForOneOpenEnd(4, myStone,board))
+      return sign*100000000;
+    if(searchForOneOpenEnd(4, opponentStone,board))
+      return -sign*50;
+
+    if(searchForTwoOpenEnd(3, myStone, board)>-1)
+      return sign*10000;
+
+    if(searchForTwoOpenEnd(3, opponentStone,board)>-1)
+      return -sign*50;
+
+    if(searchForOneOpenEnd(3, myStone,board) )
+      return sign*7;
+    if(searchForOneOpenEnd(3, opponentStone,board))
+      return -sign*5;
+
+    if(searchForTwoOpenEnd(2, myStone, board)>-1 )
+      return sign*5;
+    if(searchForTwoOpenEnd(2, opponentStone, board)>-1 )
+      return -sign*5;
+
+    if(searchForOneOpenEnd(2, myStone,board) )
+      return sign*3;
+    if(searchForOneOpenEnd(2, opponentStone,board) )
+      return -sign*3;
+
+    if(searchForTwoOpenEnd(1, myStone, board)>-1 )
+      return sign*2;
+    if(searchForTwoOpenEnd(1, opponentStone, board)>-1)
+      return -sign*2;
+    if(searchForOneOpenEnd(1, myStone,board) )
+      return sign*1;
+
+    if(searchForOneOpenEnd(1, opponentStone,board) )
+      return -sign*1;
+
+    return 200000000;
+  }
+
 }
--- lib/gameCore/minimax.dart
+++ lib/gameCore/minimax.dart
@@ -1,52 +1,123 @@
 import 'dart:math';
 
 import 'package:go_game/boardstate.dart';
-import 'package:go_game/gameCore/stoneMove.dart';
+import 'package:go_game/gameCore/goLogic.dart';
 
 class MiniMax{
 
-  int humanPlayer = 1, go_AI = 0, maxDepth = 3, boardSize = 10;
-  StoneMove optimalMove;
+  int maxDepth = 2, boardSize = 10;
+
   List<int> adjacentCellsX = [ 0, 0, 1, -1, -1, 1, -1, 1];
   List<int> adjacentCellsY = [1, -1, 0, 0, 1, -1, -1, 1];
+  static int bestMove = 0;
+  static int infinity = 99999999999999999;
 
   bool isMoveValid(int x, int y){
 
-    if((x < 0 || x >= boardSize) && (y < 0 || y >= boardSize))
-      return false;
-    else
+    if((x >= 0 && x < boardSize) && (y >= 0 && y < boardSize))
       return true;
+    else
+      return false;
   }
 
-  bool findAdjacentStone(int x, int y){
+  bool findAdjacentStone(int x, int y, List<List<int>> board){
+
 
     for(int i=0; i< adjacentCellsX.length; i++){
-        x+= adjacentCellsX[i];
-        y+= adjacentCellsY[i];
 
-        if(!isMoveValid(x, y))
+          x += adjacentCellsX[i];
+          y += adjacentCellsY[i];
+
+        if(!isMoveValid(x, y)){
           continue;
-        else if(BoardState.board[x][y] != null)
+        }
+         // continue;
+        else if(board[x][y] != null){
+          //print('${x} ${y}\n');
           return true;
+        }
     }
     return false;
   }
 
-  int evaluateGameTree(bool AI_turn){
+  int doMiniMax(int depth, bool AI_turn, int alpha, int beta, List<List<int>> board){
 
-    int profit = 0;
-    int myStone = (AI_turn == true)? 0 : 1;
+    GoLogic gl = new GoLogic();
+    int maxProfit;
 
-    if()
-  }
-  int doMiniMax(int depth, bool AI_turn, int alpha, int beta){
+    List<List<int>> gBoard = new List.generate(
+        10, (_) => new List(10));
+    for (int i = 0; i < 10; i++)
+      for (int j = 0; j < 10; j++)
+        gBoard[i][j] = board[i][j];
+
+    if(depth == 0) {
+      bool winCrucial = false, counterCrucial3 = false, counterCrucial4 =false;
+      for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+          if (!findAdjacentStone(i, j, board))
+            continue;
+
+          if (gBoard[i][j] == null) {
+
+            gBoard[i][j] = 0;
+            int winMove = gl.highFive(gBoard, 0);
+            gBoard[i][j] = null;
+
+            if(winMove>-1){
+              winCrucial = true;
+              bestMove = i*10+j;
+              return 998978798788999;
+            }
+
+            gBoard[i][j] = 1;
+            int huWin = gl.highFive(gBoard, 1);
+            int criticalMove = gl.searchForTwoOpenEnd(3, 1,gBoard);
+            bool criticalMove1 = gl.searchForOneOpenEnd(4, 1, gBoard);
+            gBoard[i][j] = null;
+
+            if(!winCrucial){
+              if(huWin>-1 && BoardState.board[i][j] == null){
+                bestMove = i*10+j;
+                print('returning human best move ${bestMove}');
+                return 9889898989598;
+              }
+              if(criticalMove>-1 && BoardState.board[i][j]==null && !counterCrucial4) {
+
+                bestMove = i*10+j;
+                print('returning human crucial 3 2 move ${bestMove}');
+                counterCrucial3 = true;
+              }
+
+              if(criticalMove1) {
+
+                bestMove = i*10+j;
+                print('returning human crucial 4 1 move ${bestMove}');
+                counterCrucial4 = true;
+              }
+            }
+          }
+        }
+      }
+      if(counterCrucial3 || counterCrucial4)
+        return 989898989598989;
+    }
+
+    if((gl.highFive(board, (AI_turn)?1:0))>-1){
+
+      if(!AI_turn)
+        return infinity+(maxDepth-depth);
+      else
+        return -infinity-(maxDepth-depth);
+    }
 
     if(depth == maxDepth){
-      return evaluateGameTree(!AI_turn);
+      return gl.evaluateGameTree(!AI_turn, board);
     }
+
       if(AI_turn){
 
-        int maxProfit = -100000000000;
+        maxProfit = -infinity;
 
         for(int i=0;i<boardSize;i++){
 
@@ -55,59 +126,88 @@ class MiniMax{
 
           for(int j=0;j<boardSize;j++){
 
-            if(!findAdjacentStone(i, j))
+            if(!findAdjacentStone(i, j, board))
               continue;
 
-            else if(BoardState.board[i][j]==null){
-              BoardState.board[i][j] = 0;
-              maxProfit = max(maxProfit, doMiniMax(depth+1, !AI_turn, alpha, beta));
-              if(maxProfit > alpha){
-                alpha = maxProfit;
-                optimalMove = new StoneMove(i, j);
-              }
-              if(alpha >= beta)
-                return maxProfit;
-            }
+            if(board[i][j]==null) {
+              List<List<int>> newBoard = new List.generate(
+                  10, (_) => new List(10));
+              for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                  newBoard[i][j] = board[i][j];
 
+              newBoard[i][j] = 0;
+
+                maxProfit = max(maxProfit,doMiniMax(depth + 1, !AI_turn, alpha, beta, newBoard));
+
+                if (maxProfit > alpha) {
+                  alpha = maxProfit;
+                  bestMove = i * 10 + j;
+                  //print('alpha ${alpha} best move ${bestMove}');
+                  //print('AI alpha ${alpha} best move ${bestMove}');
+                }
+
+                if (alpha >= beta) {
+                  return maxProfit;
+                }
+            }
           }
         }
       }
 
       if(!AI_turn){
 
-        int maxProfit = 100000000000;
+        maxProfit = infinity;
 
         for(int i=0;i<boardSize;i++){
 
           if(beta <= alpha)
-            return maxProfit;
+           return maxProfit;
 
           for(int j=0;j<boardSize;j++){
 
-            if(!findAdjacentStone(i, j))
+            if(!findAdjacentStone(i, j, board))
               continue;
 
-            else if(BoardState.board[i][j]==null){
-              BoardState.board[i][j] = 0;
-              maxProfit = max(maxProfit, doMiniMax(depth+1, !AI_turn, alpha, beta));
-              if(maxProfit  < beta){
-                beta = maxProfit;
-                optimalMove = new StoneMove(i, j);
-              }
-              if(beta <= alpha)
-                return maxProfit;
+            else if(board[i][j]==null){
+
+              List<List<int>> newBoard = new List.generate(10, (_) => new List(10));
+
+              for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                  newBoard[i][j] = board[i][j];
+
+              newBoard[i][j] = 1;
+
+              maxProfit = min(maxProfit,doMiniMax(depth+1, !AI_turn, alpha, beta, newBoard));
+              beta = min(maxProfit,beta);
+
+                if (beta <= alpha) {
+                  return maxProfit;
+                }
+
             }
 
           }
         }
       }
+      return maxProfit;
   }
-  StoneMove getOptimalMove_AI(BoardState board){
+  void getOptimalMove_AI(){
 
-    int maxProfit, alpha = -1000000000000, beta = 1000000000000, moveProfit;
     bool AI_turn= true;
+    BoardState boardState = new BoardState();
+    bestMove=0;
 
-    maxProfit = doMiniMax(0, AI_turn, alpha, beta);
+    List<List<int>> cpyBoard = new List.generate(10, (_) => new List(10));
+    for (int i = 0; i < 10; i++)
+      for (int j = 0; j < 10; j++)
+       cpyBoard[i][j] = BoardState.board[i][j];
+
+    doMiniMax(0, AI_turn, -infinity, infinity, cpyBoard);
+    if(bestMove!= -1) {
+      boardState.placeStone((bestMove / 10).toInt(), bestMove % 10, 0);
+    }
   }
 
 }
