eb399f23e224f58a2cb3a0359fac0c5ce25cdbd6
Hasnatul Jelan <bsse1026@iit.du.ac.bd>
2021-02-01 20:38:12 +0600

    Closing to an end

--- lib/boardstate.dart
+++ lib/boardstate.dart
@@ -1,17 +1,45 @@
 class BoardState {
-  static var board = new List.generate(10, (_) => new List(10));
-  static var turn = 0;
+  static List<List<int>> board = new List.generate(10, (_) => new List(10));
+  List<List<int>> tmpBoard;
+  static var turn = 1;
+  static int currentPlayer=1;
 
   void placeStone(int x, int y, int player){
     board[x][y] = player;
+    turn++;
+  }
+  /*List<List<int>> copyBoard(){
+    var cpb = new List.generate(10, (_) => new List(10));
+    for (int i = 0; i < 10; i++)
+      for (int j = 0; j < 10; j++)
+        cpb[i][j]=board[i][j];
+    return cpb;
+  }*/
+  void clearBoard(){
+    for (int i = 0; i < 10; i++)
+      for (int j = 0; j < 10; j++)
+        board[i][j]=null;
+      turn = 1;
+  }
+  int tileState(int i, int j){
+    return board[i][j];
   }
 
-  List<List<int>> getBoard(){
-    return board;
-  }
+  bool isDraw() {
+    for (int i = 0; i < 10; i++)
+      for (int j = 0; j < 10; j++)
+        if (board[i][j] == null)
+          return false;
 
+    return true;
+  }
+  int getPlayer(){
+    return (turn % 2 == 1)? 1 : 0;
+  }
+  void changePlayer(){
+    turn = (turn % 2 == 1)? 0 : 1;
+  }
   void run() {
-    print(board);
+   // print(board);
   }
 }
-
--- lib/gamepage.dart
+++ lib/gamepage.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_game/gameCore/goLogic.dart';
+import 'package:go_game/gameCore/minimax.dart';
 
 import 'boardstate.dart';
 
@@ -9,6 +13,10 @@ class Gamepage extends StatefulWidget {
 }
 
 class _GamepageState extends State<Gamepage> {
+
+  GoLogic _goLogic = new GoLogic();
+  BoardState _boardState = new BoardState();
+  bool gameOver = false;
 
   Ink chooseColor(int i) {
     if (i == 0) {
@@ -49,97 +57,174 @@ class _GamepageState extends State<Gamepage> {
 
   }
 
+  void dialogueGameOver(int player){
+    if(_goLogic.highFiveAfter(player)>-1)
+      definitiveDialogue(player);
+    if(_boardState.isDraw())
+      drawDialogue();
+  }
+
+  Object drawDialogue(){
+    gameOver = true;
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: Text('Game over'),
+          content: Text('Game Drawn!'),
+        )
+    );
+  }
+
+  Object definitiveDialogue(int playerMark){
+    String result = (playerMark == 0)? 'Lossed..' : 'Win!';
+    gameOver = true;
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: Text('Game Over'),
+          content: Text('You '+ result),
+        )
+    );
+  }
+  Future AI_makeYourMove() async{
+
+    MiniMax mnx= new MiniMax();
+    mnx.getOptimalMove_AI();
+  }
+
+  void handleGameLoop(int i, int j) async{
+
+    if(!gameOver){
+
+      if(BoardState.board[i][j] == null) {
+        _boardState.placeStone(i, j, BoardState.turn % 2);
+        dialogueGameOver(_boardState.tileState(i, j));
+      }
+
+        if(!gameOver) {
+          await AI_makeYourMove();
+          dialogueGameOver(0);
+        }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     new BoardState().run();
     return Scaffold(
       appBar: AppBar(
-        title: Text('GOMOKU'),
+        title: Text('Gomoku Redux 2021'),
       ),
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width * .97,
-            height: MediaQuery.of(context).size.height * .60,
-            child: Padding(
-              padding: EdgeInsets.only(left: 12),
-              child: Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [Color(0xffff00cc), Color(0xff333399)]),
-                        borderRadius: BorderRadius.circular(25.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black54,
-                            blurRadius: 20.0,
-                            spreadRadius: 5.0,
-                          )
-                          //offset: Offset(10.0, 20.0))
-                        ]),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        for (var i = 0; i < 10; i++)
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              for (var j = 0; j < 10; j++)
-                                Container(
-                                  child: Container(
-                                    width:
-                                    MediaQuery.of(context).size.width * .09,
-                                    height:
-                                    MediaQuery.of(context).size.width * .09,
-                                    padding: EdgeInsets.symmetric(
-                                        vertical: 2.0, horizontal: 2.0),
-                                    child: RaisedButton(
-                                      elevation: 10.0,
-                                      splashColor: Colors.white10,
-                                      child: chooseColor(BoardState.board[i][j]),
-                                      //color: Colors.red[200].withOpacity(.3),
-                                     // chooseColor(BoardState.board[i][j]),
+
+      body: Container(
+        width:MediaQuery.of(context).size.width * 1,
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            center: Alignment.bottomCenter,
+            radius: 2.5,
+            colors: [Color(0xff8f94fb),Color(0xff000428)]
+          )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            SizedBox(height: 60),
+            Container(
+              width: MediaQuery.of(context).size.width * .97,
+              height: MediaQuery.of(context).size.height * .60,
+              child: Padding(
+                padding: EdgeInsets.only(left: 5,right: 5),
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [Color(0xffff00cc), Color(0xff333399)]),
+                          borderRadius: BorderRadius.circular(25.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 20.0,
+                              spreadRadius: 5.0,
+                            )
+                            //offset: Offset(10.0, 20.0))
+                          ]),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          for (var i = 0; i < 10; i++)
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                for (var j = 0; j < 10; j++)
+                                  Container(
+                                    child: Container(
+                                      width:
+                                      MediaQuery.of(context).size.width * .09,
+                                      height:
+                                      MediaQuery.of(context).size.width * .09,
                                       padding: EdgeInsets.symmetric(
                                           vertical: 2.0, horizontal: 2.0),
-                                      shape: new RoundedRectangleBorder(
+                                      child: RaisedButton(
+                                        elevation: 10.0,
+                                        splashColor: Colors.white10,
+                                        child: chooseColor(BoardState.board[i][j]),
+                                        //color: Colors.red[200].withOpacity(.3),
+                                        // chooseColor(BoardState.board[i][j]),
+                                        padding: EdgeInsets.symmetric(
+                                            vertical: 2.0, horizontal: 2.0),
+                                        shape: new RoundedRectangleBorder(
                                           borderRadius:
                                           new BorderRadius.circular(3.0),
+                                        ),
+                                        onPressed: () {
+                                          setState(() {
+                                            /*if (BoardState.board[i][j] == null &&
+                                               BoardState.turn % 2 == 0) {
+                                             AI_makeYourMove();
+                                             dialogueGameOver(0);
+                                           } else if (BoardState.board[i][j] ==
+                                               null &&
+                                               BoardState.turn % 2 == 1) {*/
+                                            handleGameLoop(i, j);
+                                            //print(BoardState.board);
+                                            //}
+                                            //handleGameLoop(i,j);
+                                            //print(BoardState.board);
+                                          });
+                                        },
                                       ),
-                                      onPressed: () {
-                                        setState(() {
-                                          if (BoardState.board[i][j] == null &&
-                                              BoardState.turn % 2 == 0) {
-                                            BoardState.board[i][j] = 0;
-
-                                            BoardState.turn++;
-                                          } else if (BoardState.board[i][j] ==
-                                              null &&
-                                              BoardState.turn % 2 == 1) {
-                                            BoardState.board[i][j] = 1;
-                                            BoardState.turn++;
-                                          }
-                                          //print(BoardState.board);
-                                        });
-                                      },
                                     ),
-                                  ),
-                                )
-                            ],
-                          )
-                      ],
-                    ),
-                  )),
+                                  )
+                              ],
+                            )
+                        ],
+                      ),
+                    )),
+              ),
             ),
-          ),
-        ],
-      ),
+            SizedBox(height: 50,),
+            Container(
+              width: 75,
+              height: 35,
+              decoration: BoxDecoration(),
+              child: RaisedButton(
+                child: Text('Replay'),
+                color: Colors.lightBlueAccent,
+                onPressed: () {
+                  _boardState.clearBoard();
+                  setState(() {
+                    gameOver = false;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
-
